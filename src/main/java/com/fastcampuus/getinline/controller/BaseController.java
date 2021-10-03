bfda5a12bb4f941c7344901e637e63e85c1a62a1
lycigold <lycigold@naver.com>
2021-10-04 03:56:37 +0900

    add init error configurations #3
    기본에러페이지를 커스텀함

--- src/main/java/com/fastcampuus/getinline/controller/BaseController.java
+++ src/main/java/com/fastcampuus/getinline/controller/BaseController.java
@@ -1,13 +1,21 @@
 package com.fastcampuus.getinline.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController {
+public class BaseController implements ErrorController {
 
     @GetMapping("/")
     public String root() {
         return "index";
+    }
+
+    // 에러처리에 대한 제어권을 가져오기 위해서는 ErrorController를 구현해야함
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
     }
 }
