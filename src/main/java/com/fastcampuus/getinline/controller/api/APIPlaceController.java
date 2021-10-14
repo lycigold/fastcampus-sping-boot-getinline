3aef4679cb971119a78f3ebda9e6d7429ff13d13
lycigold <lycigold@naver.com>
2021-10-15 07:13:12 +0900

    Add PlaceControllerTest and Refactoring #3

--- src/main/java/com/fastcampuus/getinline/controller/api/APIPlaceController.java
+++ src/main/java/com/fastcampuus/getinline/controller/api/APIPlaceController.java
@@ -1,6 +1,9 @@
 package com.fastcampuus.getinline.controller.api;
 
-import org.springframework.stereotype.Controller;
+import com.fastcampuus.getinline.constant.PlaceType;
+import com.fastcampuus.getinline.dto.APIDataResponse;
+import com.fastcampuus.getinline.dto.PlaceDTO;
+import com.fastcampuus.getinline.dto.PlaceResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,9 +12,18 @@ import java.util.List;
 @RestController
 public class APIPlaceController {
 
+
     @GetMapping("/places")
-    public List<String> getPlaces() {
-        return List.of("place1", "place2");
+    public APIDataResponse<List<PlaceResponse>> getPlaces() {
+        return APIDataResponse.of(List.of(PlaceResponse.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로",
+                "010-1234-5678",
+                30,
+                "신장개업"
+
+        )));
     }
 
     @PostMapping("/places")
@@ -20,7 +32,21 @@ public class APIPlaceController {
     }
 
     @GetMapping("/places/{placeId}")
-    public String getPlace(@PathVariable Integer placeId) { return "place " + placeId;}
+    public APIDataResponse<PlaceDTO> getPlace(@PathVariable Integer placeId) {
+        if (placeId.equals(2)) {
+            return APIDataResponse.of(null);
+        }
+
+        return APIDataResponse.of(PlaceDTO.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로",
+                "010-1234-5678",
+                30,
+                "신장개업"
+
+        ));
+    }
 
     @PutMapping("/places/{placeId}")
     public Boolean modifyPlace(@PathVariable Integer placeId) {
---
+++ src/test/java/com/fastcampuus/getinline/dto/APIDataResponseTest.java
@@ -0,0 +1,40 @@
+package com.fastcampuus.getinline.dto;
+
+import com.fastcampuus.getinline.constant.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class APIDataResponseTest {
+
+    @DisplayName("문자열 데이터가 주어지면, 표준 성공 응답을 생성한다.")
+    @Test
+    void givenStringData_whenCreatingResponse_thenReturnsSuccessfulResponse() {
+        // Given
+        String data = "test data";
+        // When
+        APIDataResponse<String> response = APIDataResponse.of(data);
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("success", true)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
+                .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
+                .hasFieldOrPropertyWithValue("data", data);
+    }
+
+    @DisplayName("데이터가 없을때, 비어있는 표준 성공 응답을 생성한다.")
+    @Test
+    void givenNOthing_whenCreatingResponse_thenReturnsEmptySuccessfulResponse() {
+        // Given
+        String data = "test data";
+        // When
+        APIDataResponse<String> response = APIDataResponse.empty();
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("success", true)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
+                .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
+                .hasFieldOrPropertyWithValue("data", null);
+    }
+}
