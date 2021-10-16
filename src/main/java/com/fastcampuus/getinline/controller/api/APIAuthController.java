1146757e00db7927eb9d0bbd03f1cb4463a19426
lycigold <lycigold@naver.com>
2021-10-16 13:19:52 +0900

    Implement #2 - APIAuthController, dto, and its tests
    `APIAuthController` 테스트 작성 및 입출력 기본 설계
    어드민 회원가입, 로그인에 사용할 데이터 클래스 작성

--- src/main/java/com/fastcampuus/getinline/controller/api/APIAuthController.java
+++ src/main/java/com/fastcampuus/getinline/controller/api/APIAuthController.java
@@ -1,20 +1,21 @@
 package com.fastcampuus.getinline.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fastcampuus.getinline.dto.APIDataResponse;
+import com.fastcampuus.getinline.dto.AdminRequest;
+import com.fastcampuus.getinline.dto.LoginRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
 public class APIAuthController {
 
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "done.";
+    @PostMapping("/sign-up")
+    public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest) {
+        return APIDataResponse.empty();
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "done.";
+    @PostMapping("/login")
+    public APIDataResponse<String> login(@RequestBody LoginRequest loginRequest) {
+        return APIDataResponse.empty();
     }
 }
---
+++ src/main/java/com/fastcampuus/getinline/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.fastcampuus.getinline.dto;
+
+public record LoginRequest(
+        String email,
+        String password
+) {
+    public static LoginRequest of(
+            String email,
+            String password
+    ){
+        return new LoginRequest(email, password);
+    }
+}
