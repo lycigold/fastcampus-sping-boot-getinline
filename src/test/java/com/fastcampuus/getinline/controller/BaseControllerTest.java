dcbbe5f240f2bb4441a03456e7c1ab00f8ab1a1a
lycigold <lycigold@naver.com>
2021-10-16 21:41:29 +0900

    Add AuthControllerTest and update BaseControllerTest #3
    AuthController테스트 추가

--- src/test/java/com/fastcampuus/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/fastcampuus/getinline/controller/BaseControllerTest.java
@@ -3,8 +3,7 @@ package com.fastcampuus.getinline.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,8 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(BaseController.class)
 class BaseControllerTest {
 
 //    @Autowired
